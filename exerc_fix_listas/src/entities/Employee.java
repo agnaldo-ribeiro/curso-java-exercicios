af859fd76e01ce863041db3f352312c1681e7361
Agnaldo da Silva Ribeiro <agnaldoribeirodocs@gmail.com>
2026-03-23 22:50:08 -0300

    Exercicio fixação "Listas"
    Trabalhando com lista de objetos em java.
    
    curso-java-nelio-alves

---
+++ exerc_fix_listas/src/entities/Employee.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class Employee {
+	private Integer id;
+	private String name;
+	private Double salary;
+
+	public Employee(Integer id, String name, Double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+	
+	public void increaseSalary(double percentage) {
+		salary += (salary * percentage) / 100; 
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%d, %s, %.2f", id, name, salary);
+	}
+	
+	
+}
